c3d1260fda290de8e5568716eee9806ee7294669
Ben Halverson <benhalverson33@gmail.com>
2016-12-27 05:39:48 -0800

    secret quote button doesn't work

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var secretQuoteButton = document.querySelector('#secret-quote-button');
 var quoteClickStream = Rx.Observable.fromEvent(quoteButton, 'click');
 var secretQuoteClickStream = Rx.Observable.fromEvent(secretQuoteButton, 'click');
 var loginClickStream = Rx.Observable.fromEvent(loginButton, 'click');
-
 var quoteStream = quoteClickStream
   .map(function() {
     return {
@@ -19,12 +18,24 @@ var quoteResponseStream = quoteStream
   });
 
 function fetchQuote(request) {
+
+  if(request.jwt) {
+    var config = {
+      headers: {
+        "Authorization": "Bearer: " + request.jwt
+      }
+    }
+    console.log('config', config);
+  }
   return fetch(request.route)
     .then(function(data) {
       return data.text()
         .then(function(text) {
         return text;
       });
+    })
+    .catch(function(error) {
+      console.log('Error' + error);
     });
 }
 
@@ -34,6 +45,40 @@ quoteResponseStream.subscribe(function(text) {
 
 
 // Setting up Auth Streams
+Rx.Observable.prototype.authenticate = function(config) {
+  function getJwt(config) {
+    console.log('auth config', config);
+    var body;
+
+    if(config.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      body = 'username=' + config.username + '&password=' + config.password;
+
+    }
+
+    var init = {
+      method: config.method,
+      headers: config.headers,
+      body: body
+    }
+    console.log('init', init);
+
+    return fetch(config.loginPath, init)
+      .then(function(data) {
+        console.log('data', data);
+        return data.json()
+          .then(function(jwt) {
+            localStorage.setItem('id-token', jwt.id_token);
+          });
+      })
+      .catch(function(error) {
+        console.log('Error', error);
+      })
+  }
+  return this.flatMap(function(credentials) {
+    console.log('credentials', credentials);
+    return Rx.Observable.fromPromise(getJwt(credentials));
+  });
+}
 
 var loginStream = loginClickStream
   .map(function() {
@@ -51,15 +96,43 @@ var loginStream = loginClickStream
     }
   })
   .authenticate(function(config) {
+    console.log('Config', config);
     return config;
+  })
+  .catch(function(err) {
+    console.log('Error', err);
   });
 
-Rx.Observable.prototype.authenticate = function(config) {
-  function getJwt(config) {
-    var body;
 
-    if(config.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
-      body = 'username=' + config.username + '&password=' + config.password;
-    }
+
+
+Rx.Observable.prototype.authenticated = function(route) {
+  var jwt = localStorage.getItem('id-token');
+
+  // return the JWT if it exists
+  if(jwt !== undefined && jwt !== null) {
+    return this.map(function() {
+      return {
+        route: route,
+        jwt: jwt
+      }
+    });
   }
+  else return Rx.Observable.throw(new Error('No JWT in Local Storage'));
 }
+
+var secretQuoteStream = secretQuoteClickStream
+  .authenticated('http://localhost:3001/api/protected/random-quote');
+
+var quoteResponseStream = quoteStream
+  .flatMap(function(request) {
+    return fetchQuote(request);
+  })
+  .merge(secretQuoteStream
+    .flatMap(function(request) {
+      return fetchQuote(request);
+    })
+  );
+  console.log('quoteResponseStream', quoteResponseStream);
+
+  loginStream.subscribe();
